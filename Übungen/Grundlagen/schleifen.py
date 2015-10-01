fbba9d90b2c6e2d25bdb781f7ab135cd66f5c3d8
gk <gabriel.kabbe@mail.de>
2015-10-02 00:31:06 +0200

    neue übungen

---
+++ Übungen/Grundlagen/schleifen.py
@@ -0,0 +1,12 @@
+# Schleifen
+# In der Vorlesung wurde folgendes Stück Code gezeigt:
+# summe = 0
+# i = 0
+# while summe < 20:
+#   i = i + 1
+#   summe = summe + i
+#
+# Welche Werte hat die Variable summe in jedem Durchgang der while-Schleife angenommen?
+
+
+# Tatsächlich lässt sich die 
