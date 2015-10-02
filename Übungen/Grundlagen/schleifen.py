9b58d65a1422593078c03ecd5b7a521cfe9c274b
gkabbe <gabriel.kabbe@chemie.uni-halle.de>
2015-10-02 09:19:50 +0200

    Update schleifen.py

--- Übungen/Grundlagen/schleifen.py
+++ Übungen/Grundlagen/schleifen.py
@@ -7,6 +7,9 @@
 #   summe = summe + i
 #
 # Welche Werte hat die Variable summe in jedem Durchgang der while-Schleife angenommen?
+# Schreiben Sie das obige Beispiel mit einer for-Schleife
 
+# Die for-Schleife in Verbindung mit range erlaubt es, über Zahlen zu iterieren. 
+# Die range Funktion kann dabei bis zu 3 Argumente nehmen: range(start, ende, schritt)
+# Schreiben Sie eine for-Schleife, die alle ungeraden Zahlen von 1 bis 100 ausgibt.
 
-# Tatsächlich lässt sich die 
