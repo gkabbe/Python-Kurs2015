6cf5fb8955212fee3bc8276216d82f51655114d4
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2015-11-11 11:39:20 +0100

    completed atomabstände

--- Lösungen/Woche2/atomabstände.py
+++ Lösungen/Woche2/atomabstände.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+#encoding=utf-8
+
 atom_list = [] # hier werden die Atomnamen gespeichert...
 position_list = [] # und hier ihre Positionen
 with open("molecule-example.xyz", "r") as f:
@@ -9,10 +12,38 @@ with open("molecule-example.xyz", "r") as f:
 
 
 atom_counter = 0
-for atom1, position1 in enumerate(atom_list, position_list):
+# Wir iterieren über alle Atome und suchen uns die Hs raus
+for atom1, position1 in zip(atom_list, position_list):
     if atom1 == "H":
+        # Sobald wir ein H gefunden haben, speichern wir seine Position und suchen nach Sauerstoffatomen
         x1, y1, z1 = position1
-        for atom2, position2 in enumerate(atom_list, position_list):
+        for atom2, position2 in zip(atom_list, position_list):
             if atom2 == "O":
                 x2, y2, z2 = position2
-                if 
+                distance_squared = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+                if distance_squared <= 4:
+                    atom_counter += 1
+                    break # Sobald wir wissen, dass es ein nahegelegenes Sauerstoffatom gibt, müssen wir nicht weiter suchen. Die Schleife über atom2, position2 wird abgebrochen
+
+print "{} Wasserstoffatome sind nicht mehr als 2 Angström von einem Sauerstoffatom entfernt".format(atom_counter)
+
+
+
+# Jetzt das ganze für Phosphor. Diesmal müssen wir jedoch für jedes Phosphoratom zählen
+
+phosphorus_counter = 0
+# Wir iterieren über alle Atome und suchen uns die Ps raus
+for atom1, position1 in zip(atom_list, position_list):
+    if atom1 == "P":
+        # Sobald wir ein P gefunden haben, speichern wir seine Position, setzen den Sauerstoff-Zähler auf Null und suchen nach Sauerstoffatomen
+        phosphorus_counter += 1 # Optional zählen wir noch die Phosphoratome
+        oxygen_counter = 0
+        x1, y1, z1 = position1
+        for atom2, position2 in zip(atom_list, position_list):
+            if atom2 == "O":
+                x2, y2, z2 = position2
+                distance_squared = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+                if distance_squared <= 4:
+                    oxygen_counter += 1
+        print "In der Nähe von Phosphoratom Nr.{} sind {} Sauerstoffatome".format(phosphorus_counter, oxygen_counter)
+
