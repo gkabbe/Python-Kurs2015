e8a7a154a7a76af5f03299c868eaa74cfc89ca26
gk <gabriel.kabbe@mail.de>
2015-11-30 00:21:35 +0100

    updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
+cd ~
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tar.xz
 tar xvfJ Python-2.7.10.tar.xz
 cd Python-2.7.10
-./configure --prefix=~/python --enable-unicode=ucs4
+home=$(readlink -e ~)
+./configure --prefix=$home/python --enable-unicode=ucs4
 make
 make install
+
