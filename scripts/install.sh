dcba4967ba2af008473b8a5b2baae795333c5870
gk <gabriel.kabbe@mail.de>
2015-11-30 09:21:07 +0100

    tmpdir for install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,15 @@
-cd ~
+tmpd=$(mktemp -d)
+cd $tmpd
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tar.xz
 tar xvfJ Python-2.7.10.tar.xz
 cd Python-2.7.10
 home=$(readlink -e ~)
-./configure --prefix=$home/python --enable-unicode=ucs4
+./configure --prefix=$home/python2.7.10 --enable-unicode=ucs4
 make
 make install
 
 mkdir $home/virtualenvs
-virtualenv -p $home/python/bin/python2.7 $home/virtualenvs/python2.7
+virtualenv -p $home/python2.7.10/bin/python2.7 $home/virtualenvs/python2.7
 wget http://curl.haxx.se/ca/cacert.pem
 echo "alias pip=\"$home/virtualenvs/python2.7/bin/pip --cert $home/cacert.pem\"" >> $home/.bashrc
 echo "alias python=\"$home/virtualenvs/python2.7/bin/python2.7\"" >> $home/.bashrc
@@ -16,5 +17,5 @@ echo "alias ipython=\"$home/virtualenvs/python2.7/bin/ipython\"" >> $home/.bashr
 
 source $home/.bashrc
 
-./update.sh
+$home/update.sh
 
