c12824e7996749b298c27b23d5757612b0d07928
gk <gabriel.kabbe@mail.de>
2015-12-16 21:43:24 +0100

    mandelbrot

--- Lösungen/Woche5/mandelbrot.py
+++ Lösungen/Woche5/mandelbrot.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
+#encoding=utf-8
 
 import numpy as np
 from PIL import Image
 
 
 def main(*args):
-    real = np.linspace(-2, 2, 8000)
-    imag = np.linspace(-1, 1, 4000)[:, np.newaxis]
+    # An diesen Parametern darf (und soll!) gedreht werden ---------------------
+    X_MIN, X_MAX = -2.2, .5 # kleinster und größter Realteil von c
+    Y_MIN, Y_MAX = -1, 1 # kleinster und größter Imaginärteil von c
+    WIDTH = 1200 # Pixel für die Breite
+    # --------------------------------------------------------------------------
+    HEIGHT = int(float(Y_MAX-Y_MIN)/(X_MAX-X_MIN) * WIDTH) # HEIGHT ergibt sich aus der Breite und dem gewählten Bildausschnitt
+    
+    real = np.linspace(X_MIN, X_MAX, WIDTH)
+    imag = np.linspace(Y_MIN, Y_MAX, HEIGHT)[:, np.newaxis]
     c = real + 1j*imag
     
     z = np.zeros(c.shape, dtype=np.complex)
@@ -15,10 +23,12 @@ def main(*args):
     for i in xrange(100):
         z = z**2 + c
         counter += np.abs(z) < 10
+        print i, "\r",
+    print ""
         
-    counter = np.array(counter/100. * 255, dtype=np.uint8)
+    counter = np.array((counter)/100. * 255, dtype=np.uint8)
     img = Image.fromarray(counter)
-    img.save("test.png")
+    img.save("mandelbrot.png")
     
 
 if __name__ == "__main__":
